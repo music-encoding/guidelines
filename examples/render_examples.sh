994440bae2b114d097fc4f1b204e8d9656aaf677
Klaus <klaus.rettinghaus@gmail.com>
2019-11-11 17:56:10 +0100

    introduce temp dir

--- examples/render_examples.sh
+++ examples/render_examples.sh
@@ -1,14 +1,15 @@
 #--simple rendering script--#
-#--        v0.0.1         --#
+#--        v0.0.2         --#
 #--                       --#
 #--     programmed by     --#
 #--   Klaus Rettinghaus   --#
 #--                       --#
 echo rendering examples
+mkdir temp
 find . -type f -name "*.mei"|while read file;
 do
   fname=$(basename "$file")
-  saxonb-xslt -s:$file -o:${file%.mei}.ly -xsl:../mei2ly.xsl && lilypond -dresolution=120 -dno-print-pages -dpreview -fpng --loglevel=BASIC_PROGRESS ${file%.mei}.ly
+  saxonb-xslt -s:$file -o:temp/${file%.mei}.ly -xsl:../mei2ly.xsl include=init-guidelines.ily && lilypond -dresolution=120 -dno-print-pages -dpreview -fpng --loglevel=BASIC_PROGRESS temp/${file%.mei}.ly
   mv ${file%.mei}.preview.png ${file%.mei}.png
   #wrm ${file%.mei}.ly
 
