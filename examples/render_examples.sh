9ba863279d9b77fb2e27810b5df59e108a754b52
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2020-09-09 17:38:53 +0200

    update render script

--- examples/render_examples.sh
+++ examples/render_examples.sh
@@ -1,25 +1,28 @@
 #--simple rendering script--#
-#--        v0.0.3         --#
+#--        v0.0.4         --#
 #--                       --#
 #--     programmed by     --#
 #--   Klaus Rettinghaus   --#
 #--                       --#
 echo rendering examples
 mkdir temp
+
+#-- split up examples --#
 find . -type f -name "*.mei"|while read file;
 do
-  #-- split up examples --#
-  saxonb-xslt -s:$file -xsl:split-mdivs.xsl -ext:on
+  saxon -ext:on -s:$file -xsl:split-mdivs.xsl
 done
 
+#-- render examples --#
 find temp -type f -name "*.mei"|while read tempfile;
 do
-  saxonb-xslt -s:$tempfile -o:${tempfile%.mei}.ly -xsl:mei2ly.xsl include=init-guidelines.ily
+  saxon -s:$tempfile -o:${tempfile%.mei}.ly -xsl:mei2ly.xsl include=init-guidelines.ily
   lilypond -dresolution=120 -dno-print-pages -dpreview -fpng --loglevel=BASIC_PROGRESS ${tempfile%.mei}.ly
   file=$(basename "$tempfile")
   mv ${file%.mei}.preview.png ${file%.mei}.png
 done
 
+#-- clean up --#
 rm temp/*
 rmdir temp
 echo done
